c087918504a21206da6b6b0b1a8e85690c23b3c1
rabin001 <rabin.gora@gmail.com>
2019-02-19 20:24:11 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace fundamentalAlgo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //foreach loop
+            string[] arr={"civic","deified","deleveled","devoved","dew"};
+
+            Console.WriteLine("Checking words for Palindrome");
+            foreach(string str in arr)
+            {
+                Console.WriteLine("{0} = {1}" , str, AlgoPractice.isPalindrome(str));
+            }
+        }
+    }
+
+    class AlgoPractice
+    {
+        public static bool isPalindrome(string myStr)
+        {
+            for (int i=0; i<myStr.Length/2; i++)
+            {
+                if(myStr[i] != myStr[myStr.Length-1-i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
